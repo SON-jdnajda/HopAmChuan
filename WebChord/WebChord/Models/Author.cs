de8c0c91c27a67a91e23608aa94aba2cc455c4b6
Hoang Son <danghoangson2122003@gmail.com>
2024-06-12 22:15:18 +0700

    add code

---
+++ WebChord/WebChord/Models/Author.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebChord.Models;
+
+public partial class Author
+{
+    public int Id { get; set; }
+
+    public string? ComposerName { get; set; }
+
+    public string? AuthorName { get; set; }
+
+    public virtual ICollection<SongAuthor> SongAuthors { get; set; } = new List<SongAuthor>();
+}
